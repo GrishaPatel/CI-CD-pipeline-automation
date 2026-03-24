df387b484a100c808fdcee494a4bd7a4c8247602
GrishaPatel <patelgrisha30@gmail.com>
2026-03-24 19:34:49 +0530

    Fix Docker Hub username secret

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,12 +5,12 @@ echo "Starting deployment..."
 docker stop ci_cd_flask_app || true
 docker rm ci_cd_flask_app || true
 
-docker pull PatelGrisha/ci-cd-pipeline:latest
+docker pull patelgrisha/ci-cd-pipeline:latest
 
 docker run -d \
   --name ci_cd_flask_app \
   -p 5000:5000 \
-  PatelGrisha/ci-cd-pipeline:latest
+  patelgrisha/ci-cd-pipeline:latest
 
 echo "Deployment completed successfully!"
 
